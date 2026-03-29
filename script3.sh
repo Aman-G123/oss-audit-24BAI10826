3637c023822e59a01db1cafb577ac3490b727868
Aman <amangarg2501@gmail.com>
2026-03-29 11:56:22 +0530

    Add Disk and Permission Auditor script
    This script checks the size, ownership, and permissions of specified directories.

--- script3.sh
+++ script3.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# Script 3: Disk and Permission Auditor
+# Author: Aman Garg
+# Description: This script checks directory size, ownership, and permissions.
+
+# List of directories to check
 DIRS=("/etc" "/home" "/usr/bin" "/tmp")
 
 echo "Directory Audit Report"
 echo "----------------------"
 
+# Loop through directories
 for DIR in "${DIRS[@]}"; do
     if [ -d "$DIR" ]; then
+        
+        # Get permissions and owner
         PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        
+        # Get directory size
         SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        
+        # Display result
         echo "$DIR => Permissions: $PERMS | Size: $SIZE"
     else
         echo "$DIR does not exist"
